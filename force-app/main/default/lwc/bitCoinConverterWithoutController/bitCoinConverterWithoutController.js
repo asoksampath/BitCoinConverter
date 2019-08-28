fa81e61cbdbab94288bd399656e61e4ad0702811
asoksampath@gmail.com <424004@cognizant.com>
2019-08-28 18:04:27 +0400

    completed the functionality within JS

--- force-app/main/default/lwc/bitCoinConverterWithoutController/bitCoinConverterWithoutController.js
+++ force-app/main/default/lwc/bitCoinConverterWithoutController/bitCoinConverterWithoutController.js
@@ -1,5 +1,8 @@
+/* eslint-disable no-alert */
 import { LightningElement, track } from 'lwc';
 import BTC from '@salesforce/label/c.BTC';
+import createRecordForComparison from '@salesforce/apex/BitCoin_Controller.createRecordForComparison';
+     
 
 export default class BitCoinConverterWithoutController extends LightningElement 
 {
@@ -13,8 +16,10 @@ export default class BitCoinConverterWithoutController extends LightningElement
     @track BitCoinValue = 1;
     @track otherValue = 0;
     selectedOption;
+    bitValue;
+    otherCurrValue;
     @track selectedYear;
-    //isPopulated="false";
+    mapOfCurrency = new Map();
 
     constructor()
     {
@@ -23,27 +28,70 @@ export default class BitCoinConverterWithoutController extends LightningElement
         .then(function(response) {
             return response.json();
         })
-        .then((myJson) => {
+        .then((myJson) => 
+        {
             for(let i=0; i < myJson.length; i++)
             {
                 this.listItems = [...this.listItems,{value: myJson[i].code , label: myJson[i].code + '-' + myJson[i].name} ];                        
+                this.mapOfCurrency.set(myJson[i].code, myJson[i]);
             }
             this.error = undefined;
         })
         .catch(e=>window.console.log('error is '+ e));
     }
 
+
     get currencyList()
-    {   
+    {
         return this.listItems;
     }   
     
-    //To calculate while the currency code changes
-    handleChange(event) {
-        // Get the string of the "value" attribute on the selected option
-        this.selectedOption = event.detail.value;
+    //to calculate other value while changing bit coin value
+    handleCurrencyValue(event)
+    {
+        this.bitValue = event.detail.value;
+        if(this.selectedOption === undefined || this.bitValue === "NaN" || this.selectedOption === "-Selelct-" )
+        {
+            alert("Please select currency");
+        }
+        else{
+            this.calculateCurrencyValue(this.selectedOption, this.bitValue);
+        }
+               
     }
 
+    //To calculate while the currency code changes
+    handleChange(event) 
+    {
+        // Get the string of the "value" attribute on the selected option
+        this.selectedOption = event.detail.value;    
+        this.calculateCurrencyValue(this.selectedOption,this.BitCoinValue);
+    }
+
+
+    //to calculate other value
+    calculateCurrencyValue(selectedCurrency, bitcoin)
+    {
+        this.otherValue = this.mapOfCurrency.get(selectedCurrency).rate * bitcoin ;
+        /*
+        const allValid = [...this.template.querySelectorAll('lightning-input')]
+            .reduce((validSoFar, inputCmp) => {
+                        inputCmp.reportValidity();
+                        return validSoFar && inputCmp.checkValidity();
+            }, true);
+        if (allValid) {
+        }   
+        */   
+    }
+
+    //to calculate bitcoinvalue
+    handleBitCoinValue(event)
+    {
+        this.otherCurrValue = event.detail.value;
+        this.BitCoinValue = this.otherCurrValue / this.mapOfCurrency.get(this.selectedOption).rate;
+        createRecordForComparison({bitCoinValue: this.BitCoinValue, otherCurrency:this.selectedOption , otherCurrencyValue: this.otherCurrValue, rep: BTC + ' to ' + this.selectedOption});
+    }
+    /*
     get yearsList()
     {   
         var d = new Date();
@@ -71,4 +119,5 @@ export default class BitCoinConverterWithoutController extends LightningElement
             { label: 'Finished', value: 'finished' },
         ];
     }
+    */
 }
